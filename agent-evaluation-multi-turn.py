b4920520d2d6e3112a173217a557da263ff09010
ayushijamicrosoft <140367063+ayushijamicrosoft@users.noreply.github.com>
2025-09-18 17:20:32 +0530

    Comment out evaluation data preparation code
    Comment out evaluation data preparation and print statements.

--- agent-evaluation-multi-turn.py
+++ agent-evaluation-multi-turn.py
@@ -68,17 +68,17 @@ thread_id = thread.id
 # converted_data = converter.convert(thread_id=thread_id, run_id="run_MhHx17PIXfVDvcbil0aM8zrb")
 # print(json.dumps(converted_data, indent=4))
 
-file_name = "freshEvaluationData.jsonl"
-evaluation_data = converter.prepare_evaluation_data(thread_ids=thread.id, filename=file_name)
+# file_name = "freshEvaluationData.jsonl"
+# evaluation_data = converter.prepare_evaluation_data(thread_ids=thread.id, filename=file_name)
 
-print(evaluation_data)
+# print(evaluation_data)
 
 
 load_dotenv()
 from azure.ai.evaluation import ToolCallAccuracyEvaluator , AzureOpenAIModelConfiguration, IntentResolutionEvaluator, TaskAdherenceEvaluator, ViolenceEvaluator
 from pprint import pprint
 for run in list_of_runs:
-    
+    print(run.as_dict())
     #converted_data = converter.convert(thread_id=thread_id, run_id=run.id)
 
     #print(converted_data)
