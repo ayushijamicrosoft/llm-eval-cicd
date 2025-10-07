8c162068dfcfd0787b96435034698b8ce4a28935
ayushijamicrosoft <140367063+ayushijamicrosoft@users.noreply.github.com>
2025-10-07 15:43:25 +0530

    Update EndToEndAgentEvaluation.py

--- EndToEndAgentEvaluation.py
+++ EndToEndAgentEvaluation.py
@@ -20,15 +20,10 @@ import pprint
 from azure.ai.evaluation import ToolCallAccuracyEvaluator , AzureOpenAIModelConfiguration, IntentResolutionEvaluator, TaskAdherenceEvaluator,RelevanceEvaluator, CoherenceEvaluator, FluencyEvaluator, ViolenceEvaluator, SelfHarmEvaluator, SexualEvaluator, HateUnfairnessEvaluator, CodeVulnerabilityEvaluator, IndirectAttackEvaluator, ProtectedMaterialEvaluator
 from pprint import pprint
 
-
 # Initialize the environment variables for Azure OpenAI and the AI project details
 openai_endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
 model_name = os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT")
 deployment = os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT")
-
-print(openai_endpoint)
-print(model_name)
-print(deployment)
 
 openai_key = os.environ.get("AZURE_OPENAI_API_KEY")
 api_version = os.environ.get("AZURE_OPENAI_API_VERSION")
@@ -39,9 +34,9 @@ os.environ["AZURE_API_VERSION"] = api_version
 os.environ["AZURE_IDENTITY_ENABLE_INTERACTIVE"] = "1"
 
 azure_ai_project = {
-    "subscription_id": "40449e6d-a5d2-40f1-a151-0b76f21a48c0",
-    "project_name": "ai-foundry-hack25",
-    "resource_group_name": "rg-padmajat-3654",
+    "subscription_id": "49d64d54-e966-4c46-a868-1999802b762c",
+    "project_name": "padmajat-agenticai-hackathon25",
+    "resource_group_name": "rg-padmajat-2824",
 }
 
 credential=DefaultAzureCredential()
@@ -56,56 +51,28 @@ model_config = {
     "azure_deployment": deployment,
 }
 
+def example_application_response(query: str, context: str) -> str:
+    deployment = os.environ.get("AZURE_DEPLOYMENT_NAME")
+    endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
 
-load_dotenv()
+    # Get a client handle for the AOAI model
+    client = AzureOpenAI(
+        azure_endpoint=endpoint,
+        api_version=os.environ.get("AZURE_API_VERSION"),
+        api_key=openai_key
+    )
 
-from azure.ai.projects import __version__ as projects_version
-from packaging.version import Version
-# some dependencies have been updated with breaking changes -- indicates whether to use updated models and APIs or not
-updated_agents = Version(projects_version) > Version("1.0.0b10") or projects_version.startswith("1.0.0a")
-
-credential=DefaultAzureCredential()
-
-from azure.ai.agents.models import FunctionTool, ToolSet
-project_client = AIProjectClient(
-    endpoint="https://ai-foundry-hack25.services.ai.azure.com/api/projects/AIFoundryHack25",
-    credential=DefaultAzureCredential()
-)
-
-agent = project_client.agents.get_agent(
-    agent_id = "asst_jLLKNsxIecXELw3ICgNquvmo"
-)
-
-list_of_prompts = [
-  "Can you list information for any 10 sales orders in my system?"
-]
-print(f"Fetched agent, ID: {agent.id}")
-print("LIST OF PROMPTS")
-print(list_of_prompts)
-count = 0 
-for prompt in list_of_prompts:
-    try:
-        count += 1; 
-        thread = project_client.agents.threads.create()
-        print(f"Created thread, ID: {thread.id}")
-        
-        # Create message to thread
-        
-        MESSAGE = prompt
-        
-        message = project_client.agents.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=MESSAGE,
-        )
-            
-        print(f"Created message, ID: {message.id}")
-        
-        from azure.ai.agents.models import (
-            FunctionTool,
-            ListSortOrder,
-            RequiredFunctionToolCall,
-            SubmitToolOutputsAction,
+    # Prepare the messages
+    messages = [
+        {
+            "role": "system",
+            "content": f"You are a user assistant who helps answer questions based on some context.\n\nContext: '{context}'",
+        },
+        {"role": "user", "content": query},
+    ]
+    # Call the model
+    completion = client.chat.completions.create(
+        model=deployment,
             ToolOutput,
         )
         run = project_client.agents.runs.create(thread_id=thread.id, agent_id=agent.id)
@@ -174,7 +141,14 @@ for prompt in list_of_prompts:
         print("==============================================================CONVERTED DATA===========================================================================")
         converted_data = converter.convert(thread_id=thread_id, run_id=run_id)
         print(converted_data)
-        print("-------------------------------------------------------------------------------------------------------------------------------------------------------")
+        # Save the converted data to a JSONL file
+    
+        file_name = "evaluationDataAdverserialData" + str(count) + ".jsonl"
+        evaluation_data = converter.prepare_evaluation_data(thread_ids=thread.id, filename=file_name)
+        
+        load_dotenv()
+        
+        
         model_config = AzureOpenAIModelConfiguration(
             azure_endpoint=openai_endpoint,
             api_key=openai_key,
@@ -183,32 +157,70 @@ for prompt in list_of_prompts:
         )
         # Needed to use content safety evaluators
         azure_ai_project={
-             "subscription_id": "40449e6d-a5d2-40f1-a151-0b76f21a48c0",
-              "project_name": "ai-foundry-hack25",
-              "resource_group_name": "rg-padmajat-3654",
+            "subscription_id": "49d64d54-e966-4c46-a868-1999802b762c",
+                  "project_name": "padmajat-agenticai-hackathon25",
+                  "resource_group_name": "rg-padmajat-2824",
         }
-        file_name = "evaluation_data_A.jsonl"
-        evaluation_data = converter.prepare_evaluation_data(thread_ids=thread.id, filename=file_name)
-
-        load_dotenv()
         
-    
         tool_call_accuracy = ToolCallAccuracyEvaluator(model_config=model_config)
+        intent_resolution = IntentResolutionEvaluator(model_config=model_config)
+        task_adherence = TaskAdherenceEvaluator(model_config=model_config)
+        relevance = RelevanceEvaluator(model_config=model_config)
+        coherence = CoherenceEvaluator(model_config=model_config)
+        fluency = FluencyEvaluator(model_config=model_config)
+        violence = ViolenceEvaluator(credential=credential, azure_ai_project=azure_ai_project)
+        self_harm = SelfHarmEvaluator(credential=credential, azure_ai_project=azure_ai_project)
+        sexual = SexualEvaluator(credential=credential, azure_ai_project=azure_ai_project)
+        hate_unfairness= HateUnfairnessEvaluator(credential=credential, azure_ai_project=azure_ai_project)
+        code_vulnerability = CodeVulnerabilityEvaluator(credential=credential, azure_ai_project=azure_ai_project)
+        indirect_attack = IndirectAttackEvaluator(credential=credential, azure_ai_project=azure_ai_project)
+        protected_material = ProtectedMaterialEvaluator(credential=credential, azure_ai_project=azure_ai_project)
+        
         
         tool_call_accuracy(query=converted_data['query'], response=converted_data['response'], tool_definitions=converted_data['tool_definitions'])
-    
+        intent_resolution(query=converted_data['query'], response=converted_data['response'])
+        task_adherence(query=converted_data['query'], response=converted_data['response'])
+        violence(query=converted_data['query'], response=converted_data['response'])
+        relevance(query=converted_data['query'], response=converted_data['response'])
+        coherence(query=converted_data['query'], response=converted_data['response'])
+        fluency(response=converted_data['response'])
+        self_harm(query=converted_data['query'], response=converted_data['response'])
+        sexual(query=converted_data['query'], response=converted_data['response'])
+        hate_unfairness(query=converted_data['query'], response=converted_data['response'])
+        code_vulnerability(query=converted_data['query'], response=converted_data['response'])
+        indirect_attack(query=converted_data['query'], response=converted_data['response'])
+        protected_material(query=converted_data['query'], response=converted_data['response'])
+        
+        
+        from azure.ai.evaluation import evaluate
+        
         response = evaluate(
             data=file_name,
             evaluators={
-                "tool_call_accuracy": tool_call_accuracy
+                "tool_call_accuracy": tool_call_accuracy,
+                "intent_resolution": intent_resolution,
+                "task_adherence": task_adherence,
+                "violence": violence,
+                "relevance": relevance,
+                "coherence": coherence,
+                "fluency": fluency,
+                "self_harm": self_harm,
+                "sexual": sexual,
+                "hate_unfairness": hate_unfairness,
+                "code_vulnerability": code_vulnerability,
+                "indirect_attack": indirect_attack,
+               "protected_material": protected_material
             },
             azure_ai_project={
-                 "subscription_id": "49d64d54-e966-4c46-a868-1999802b762c",
+                "subscription_id": "49d64d54-e966-4c46-a868-1999802b762c",
                   "project_name": "padmajat-agenticai-hackathon25",
                   "resource_group_name": "rg-padmajat-2824",
             }
         )
-        pprint(f'AI Foundary URL: {response.get("studio_url")}')
+        from pprint import pprint
+    
+        pprint(list_of_prompts, width=200)
+        pprint(f'Azure ML Studio URL: {response.get("studio_url")}')
         pprint(response)
         
         # Save evaluation response to JSON file
@@ -216,6 +228,6 @@ for prompt in list_of_prompts:
             json.dump(response, f, indent=2, default=str)
     
     except Exception as exception:
-            print("exception occured!")
-            print(exception)
-            continue;
+        print("exception occured!")
+        print(exception)
+        continue;
