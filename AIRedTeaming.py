d5952da31de49d9c097e8707ae26d4c92238003e
ayushijamicrosoft <140367063+ayushijamicrosoft@users.noreply.github.com>
2025-11-17 19:44:09 +0530

    fix indent

--- AIRedTeaming.py
+++ AIRedTeaming.py
@@ -470,10 +470,10 @@ def process_prompts_with_agent(
     random_number  = random.randint(1, 50)
     if prompt_records == 0:
         return;
-     desired_eval_names = SIMULATOR_EVALUATOR_MAP.get(
-         prompt_records[0].simulator,
-         list(evaluator_map.keys()),
-     )
+    desired_eval_names = SIMULATOR_EVALUATOR_MAP.get(
+        prompt_records[0].simulator,
+        list(evaluator_map.keys()),
+    )
     # Only require that the evaluator actually exists
     selected_eval_names = [
         name
