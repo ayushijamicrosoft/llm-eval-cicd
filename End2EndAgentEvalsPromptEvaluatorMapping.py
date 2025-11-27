3bbb85344ae6ca172fd5497469fa5bed09b42bed
vchinnu <127180385+vchinnu@users.noreply.github.com>
2025-11-27 12:03:07 +0530

    Fix formatting issues in End2EndAgentEvalsPromptEvaluatorMapping.py

--- End2EndAgentEvalsPromptEvaluatorMapping.py
+++ End2EndAgentEvalsPromptEvaluatorMapping.py
@@ -90,8 +90,10 @@ SIMULATOR_EVALUATOR_MAP = {
      "custom": [
         "tool_call_accuracy", "intent_resolution", "task_adherence",
         "relevance", "coherence", "fluency", "indirect_attack"
+       '''
         "code_vulnerability", "ungrounded_attributes","violence", "self_harm", "sexual",
         "hate_unfairness", "protected_material", "ungrounded_attributes"
+       '''
     ],
 }
 
